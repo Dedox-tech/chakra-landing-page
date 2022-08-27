cb8d79fdc0d9e3127210ca8077cdce1798b56b00
Diego Murillo Ferrer <diegomurilloferrer@gmail.com>
2022-08-27 18:12:45 -0500

    Add first About us componenet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import MapPage from "./pages/MapPage";
+import AboutUs from "./components/AboutUs";
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
             <Route path="/" element={<MainPage />} />
             <Route path="*" element={<MainPage />} />
             <Route path="/map" element={<MapPage />} />
+            <Route path="/about-us" element={<AboutUs/>}/>
         </Routes>
     );
 }
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,12 +3,14 @@ import { Box } from "@chakra-ui/react";
 import CustomHeader from "../components/CustomHeader";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import AboutUs from "../components/AboutUs";
 
 export default function MainPage() {
     return (
         <Box>
             <NavBar />
             <CustomHeader />
+            <AboutUs/>
             <Footer />
         </Box>
     );
