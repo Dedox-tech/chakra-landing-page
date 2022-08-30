9701eebd7d29e13342c2550b7c7dba5c8a301d01
Jose Lanza <jlanza@highroads.com>
2022-08-30 07:59:58 -0400

    adding content

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,9 +1,35 @@
-import React from "react";
-import { Box, Heading, Text , Container, useBreakpointValue } from "@chakra-ui/react";
+import {React, useState} from "react";
+import { useNavigate} from 'react-router-dom';
+
+import { Box, Heading, FormControl, Container, useBreakpointValue, FormLabel, Input, FormHelperText, Button, Textarea, Divider } from "@chakra-ui/react";
+import { EmailIcon } from "@chakra-ui/icons";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
+
 export default function ContactUs() {
+
+    const navigate = useNavigate();
+
+    const [message, setMessage] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const setMessageValue = event => {
+        setMessage(event.target.value);
+      };
+
+      const setFullNameValue = event => {
+        setFullName(event.target.value);
+      };
+
+      const setEmailValue = event => {
+        setEmail(event.target.value);
+      };
+
+    const onClickSend = () =>{
+        navigate("/map");
+    }
 
     const customMaxWidth = useBreakpointValue({
         xl: "container.lg",
@@ -14,12 +40,43 @@ export default function ContactUs() {
     return (
         <Box>
             <NavBar />
-            <Container maxWidth={customMaxWidth}  centerContent> 
-            <Heading as="h1" align="center">Contact Us</Heading>
-                <Text>Esta es una prueba</Text>
-            <Footer />
+            <Container maxWidth={customMaxWidth} centerContent>
+                <Heading as="h1" align="center">Contact Us</Heading>
+                <FormControl>
+                    <FormLabel>Full Name</FormLabel>
+                    <Input type='Text' onChange={setFullNameValue} values={fullName} />
+                </FormControl>
+                <FormControl>
+                    <FormLabel>Email address</FormLabel>
+                    <Input type='email' onChange={setEmailValue} values={email} />
+                    <FormHelperText>We ll never share your email.</FormHelperText>
+                </FormControl>
+
+                <FormControl>
+                    <FormLabel>Message</FormLabel>
+                    <Textarea placeholder="Write your message..." onChange={setMessageValue} values={message} />
+                </FormControl>
+                <FormControl>
+                <Divider/>
+                </FormControl>
+
+                {"\n"}
+
+                <FormControl align="right" m={4}>
+                <Button bg={Footer.tealLightOrTealDarkColor}
+                            color={Footer.whiteOrGrayColor}
+                            leftIcon={<EmailIcon />}
+                            colorScheme='teal'
+                            _hover={{ bg: "teal.600" }}
+                            onClick={onClickSend}
+                        >Send</Button>
+                </FormControl>
+                
+
             </Container>
 
+
+            <Footer />
         </Box>
     );
 }
