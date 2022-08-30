97a5f53a39fae8af295ae904b3111448f8b2554b
Diego Murillo <diegomurilloferrer@gmail.com>
2022-08-30 18:57:01 -0500

    Add header in the map and make it full-width

--- src/components/NewCustomMap.js
+++ src/components/NewCustomMap.js
@@ -1,28 +1,11 @@
 /* eslint-disable new-cap */
-import { Box, Container } from "@chakra-ui/react";
-import React, {useState, useEffect} from "react";
-// import {/*Box, Container, Heading,*/ useBreakpointValue} from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
 import svgMap from "svgmap";
 import "../svgMap.css";
 
 export default function NewCustomMap() {
-
     const [doesMapExist, setDoesMapExist] = useState(false);
 
-    /*
-    const customMaxWidth = useBreakpointValue({
-        xl: "container.lg",
-        lg: "4xl",
-        md: "2xl",
-    });
-
-    const customPadding = useBreakpointValue({
-        xl: 24,
-        lg: 20,
-        md: 12,
-        base: 6,
-    });
-    */
     useEffect(() => {
         if (!doesMapExist) {
             // eslint-disable-next-line no-unused-vars, prefer-const
@@ -30,58 +13,50 @@ export default function NewCustomMap() {
                 targetElementID: "svgMap",
                 data: {
                     data: {
-                      moderna: {
-                        name: 'Moderna Vaccines',
-                        format: '{0} dosis',
-                        thousandSeparator: ',',
-                        thresholdMax: 50000,
-                        thresholdMin: 0
-                      },
-                      jhonson_jhonson: {
-                        name: 'Jhonson and Jhonson Vaccines',
-                        format: '{0} dosis',
-                        thresholdMax: 50000,
-                        thresholdMin: 0
-                      },
-                      sinopharma: {
-                        name: 'Sinopharma Vaccines',
-                        format: '{0} dosis',
-                        thresholdMax: 50000,
-                        thresholdMin: 0
-                      }
+                        moderna: {
+                            name: "Moderna Vaccines",
+                            format: "{0} dosis",
+                            thousandSeparator: ",",
+                            thresholdMax: 50000,
+                            thresholdMin: 0,
+                        },
+                        jhonson_jhonson: {
+                            name: "Jhonson and Jhonson Vaccines",
+                            format: "{0} dosis",
+                            thresholdMax: 50000,
+                            thresholdMin: 0,
+                        },
+                        sinopharma: {
+                            name: "Sinopharma Vaccines",
+                            format: "{0} dosis",
+                            thresholdMax: 50000,
+                            thresholdMin: 0,
+                        },
                     },
-                    applyData: 'moderna',
+                    applyData: "moderna",
                     values: {
-                      "AF": { "moderna": 587, "jhonson_jhonson": 473, "sinopharma" : 334 },
-                      "AL": { "moderna": 4583, "jhonson_jhonson": 1109, "sinopharma" : 334 },
-                      "DZ": { "moderna": 4293, "jhonson_jhonson": 1001, "sinopharma" : 0 }
-                      // ...
-                    }
-                  }
+                        AF: {
+                            moderna: 587,
+                            jhonson_jhonson: 473,
+                            sinopharma: 334,
+                        },
+                        AL: {
+                            moderna: 4583,
+                            jhonson_jhonson: 1109,
+                            sinopharma: 334,
+                        },
+                        DZ: {
+                            moderna: 4293,
+                            jhonson_jhonson: 1001,
+                            sinopharma: 0,
+                        },
+                        // ...
+                    },
+                },
             });
             setDoesMapExist(true);
         }
-
     }, [doesMapExist]);
 
-    return (
-      /*
-        <Container maxWidth={customMaxWidth} py={customPadding} centerContent>
-            <Heading as="h1" mb={8}>
-                Meet our beautiful map!
-            </Heading>
-            <Box id = "svgMap" />
-        </Container>
-        */
-       <Container>
-       <Box>
-       <div className='app'>
-        <h1>svgMap React demo</h1>
-        <div id='svgMap'> </div>
-      </div>
-       </Box>
-        
-      </Container>
-    );
-
+    return <div id="svgMap"> </div>;
 }
--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
 import NewCustomMap from "../components/NewCustomMap";
+import MapHeader from "../components/MapHeader";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
@@ -8,6 +9,7 @@ export default function MapPage() {
     return (
         <Box>
             <NavBar />
+            <MapHeader />
             <NewCustomMap />
             <Footer />
         </Box>
