30700c89387dc840f9c0be25559f68f51e659659
Diego Murillo <diegomurilloferrer@gmail.com>
2022-08-31 22:12:56 -0500

    Add the FAQ component

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,12 +3,14 @@ import { Box } from "@chakra-ui/react";
 import CustomHeader from "../components/CustomHeader";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import Faq from "../components/Faq";
 
 export default function MainPage() {
     return (
         <Box>
             <NavBar />
             <CustomHeader />
+            <Faq />
             <Footer />
         </Box>
     );
