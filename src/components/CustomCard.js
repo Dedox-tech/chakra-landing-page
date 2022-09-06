931a72a09b1464e8bae4960802ef94939600e7ee
Diego Murillo Ferrer <diegomurilloferrer@gmail.com>
2022-09-05 23:44:43 -0500

    Spin up the first card of the news section

--- src/components/CustomCard.js
+++ src/components/CustomCard.js
@@ -1,12 +1,17 @@
 import React from "react";
-import {Box, Text} from "@chakra-ui/react";
+import { Box, Image, Stack, Text, Heading } from "@chakra-ui/react";
+import Image1 from "../images/Image-1.jpg";
 
-export default function CustomCard () {
+export default function CustomCard() {
     return (
-        <Box>
-            <Text>
-                Hello world!
-            </Text>
+        <Box maxW="350px" boxShadow="md">
+            <Box h="220px">
+                <Image src={Image1} alt="Covid-19 Image" layout="fill" />
+            </Box>
+            <Stack>
+                <Text>Hello world!</Text>
+                <Heading>I am a heading</Heading>
+            </Stack>
         </Box>
     );
 }
