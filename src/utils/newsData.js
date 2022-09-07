198f04cdb549e123090e63e9ae46eef9420ab608
Diego Murillo Ferrer <diegomurilloferrer@gmail.com>
2022-09-06 23:38:49 -0500

    Add data for the news content

---
+++ src/utils/newsData.js
@@ -0,0 +1,105 @@
+import Image1 from "../images/Image-1.jpg";
+import Image2 from "../images/Image-2.jpg";
+import Image3 from "../images/Image-3.jpg";
+import Image4 from "../images/Image-4.jpg";
+import Image5 from "../images/Image-5.jpg";
+import Image6 from "../images/Image-6.jpg";
+import Image7 from "../images/Image-7.jpg";
+import Image8 from "../images/Image-8.jpg";
+
+const newsData = [
+    {
+        country: "United States of America",
+        heading: "New omicron boosters are now available at U.S.",
+        description:
+            "The U.S. authorized the first major makeover of the Covid-19 vaccines this week in an effort to stem an expected tide of infections and hospitalizations this fall.",
+        author: "William Raff",
+        date: "Sep, 06, 2022",
+        timeInMins: "4",
+        image: Image1,
+        imageAlt: "An ilustration of the Coronavirus",
+    },
+    {
+        country: "China",
+        heading: "China approves first inhaled Covid vaccine",
+        description:
+            "China has become the first country to green-light an inhaled Covid-19 vaccine, paving the way for potential use of the needle-free product in the country, where suppressing the spread of Covid-19 remains a top priority.",
+        author: "Rose Hutchinson",
+        date: "Sep, 06, 2022",
+        timeInMins: "8",
+        image: Image2,
+        imageAlt: "A group of Covid-19 vaccines",
+    },
+    {
+        country: "Canada",
+        heading:
+            "What protection to expect from updated COVID vaccines this fall",
+        description:
+            "An updated Moderna vaccine is expected to roll out in Canada within days, and researchers will be watching how much added protection it offers.",
+        author: "Adam Miller",
+        date: "Sep, 05, 2022",
+        timeInMins: "6",
+        image: Image3,
+        imageAlt: "Biosecurity elements for covid-19",
+    },
+    {
+        country: "United Kingdom",
+        heading:
+            "First bivalent COVID-19 booster vaccine approved by UK medicines regulator",
+        description:
+            "The adapted COVID-19 vaccine made by Moderna targets two different coronavirus variants: the original virus from 2020 and the Omicron variant.",
+        author: "Larry Parker",
+        date: "Sep, 04, 2022",
+        timeInMins: "12",
+        image: Image4,
+        imageAlt: "A doctor getting ready for her shift",
+    },
+    {
+        country: "Switzerland",
+        heading: "Interim statement on COVID-19 vaccination for children",
+        description:
+            "WHO is currently reviewing the emerging evidence on the need for and timing of vaccinating children and adolescents with the currently available COVID-19 vaccines, which have received Emergency Use Listing (EUL).",
+        author: "World Health Organization",
+        date: "Sep, 03, 2022",
+        timeInMins: "16",
+        image: Image5,
+        imageAlt: "A child wearing a mask",
+    },
+    {
+        country: "United States of America",
+        heading:
+            "Biden administration is preparing for the end of free COVID-19",
+        description:
+            "The federal budget for buying and distributing COVID-19 vaccines will run out as early as January.",
+        author: "Lucy Vargas",
+        date: "Sep, 03, 2022",
+        timeInMins: "7",
+        image: Image6,
+        imageAlt: "A nurse giving an injection",
+    },
+    {
+        country: "Spain",
+        heading: "COVID-19 vaccine campaigns could be faster, industry says",
+        description:
+            "Speed is of the essence to protect citizens from new COVID-19 outbreaks this fall, but the process to distribute more vaccines must be accelerated, according to pharmaceutical industry representatives.",
+        author: "Isabel Silva",
+        date: "Sep, 02, 2022",
+        timeInMins: "4",
+        image: Image7,
+        imageAlt: "A young girl washing her hands",
+    },
+    {
+        country: "Israel",
+        heading:
+            "Data from Israel: myocarditis after COVID-19 vaccines remain rare",
+        description:
+            "A new study from Israel found that the risk of developing myocarditis among males ages 16 to 19 years was about 1 in 15,000 after third dose of the Pfizer COVID-19 vaccine",
+        author: "John Davis",
+        date: "Sep, 06, 2022",
+        timeInMins: "22",
+        image: Image8,
+        imageAlt: "A man wearing a mask. The man is a little worried",
+    },
+];
+
+export default newsData;
