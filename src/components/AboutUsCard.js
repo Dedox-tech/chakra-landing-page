a3d32865448282f961ba73c52b458fe7255e57a4
Diego Murillo Ferrer <diegomurilloferrer@gmail.com>
2022-09-12 18:37:41 -0500

    Add new about us card

---
+++ src/components/AboutUsCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, Text, Avatar, Heading } from "@chakra-ui/react";
+import Diego from "../images/Diego-Murillo.png";
+
+export default function AboutUsCard() {
+    return (
+        <Box borderWidth="1px" p={5}>
+            <Avatar src={Diego} size="2xl" borderWidth="1px" />
+            <Box mt={2}>
+                <Heading as="h6" size="md">
+                    Diego Murillo Ferrer
+                </Heading>
+                <Text color="gray.500">23 years old &bull; Colombian</Text>
+            </Box>
+            <Text mt={2}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Praesent malesuada finibus nunc, ut laoreet mauris lacinia id.
+                Nam sit amet nibh eu dolor placerat placerat.
+            </Text>
+        </Box>
+    );
+}
--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
-
 import NavBar from "../components/NavBar";
-import AboutUs from "../components/AboutUs";
 import Footer from "../components/Footer";
+import AboutUsCard from "../components/AboutUsCard";
 
 export default function MainPage() {
     return (
         <Box>
             <NavBar />
-            <AboutUs />
+            <AboutUsCard />
             <Footer />
         </Box>
     );
